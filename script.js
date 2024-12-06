d385c72eb3f4a3c1d1855378fcf16bb4e6f33b95
phzinx18 <phmoraesderodrigues08@gmail.com>
2024-12-06 09:34:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function darkMode() {
+    document.body.classList.toggle("dark-theme");
+
+    const logo = document.getElementById("logo");
+
+
+    if (document.body.classList.contains("dark-theme")) {
+        //   Troca a logo para a versão escura
+        logo.src = "src/Logo modo escuro.png";
+        logof.src = "src/Logo modo escuro.png"
+    } else {
+        // Troca a logo para a versão clara
+        logo.src = "src/Logo modo claro.png";
+        logof.src = "src/Logo modo claro.png";
+    }
+}
