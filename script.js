5a44e6a35e69cc7aae869c0fcdecfd4c3d827d8e
phzinx18 <phmoraesderodrigues08@gmail.com>
2024-12-10 11:52:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+
+hamburgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
 function darkMode() {
     document.body.classList.toggle("dark-theme");
 
     const logo = document.getElementById("logo");
-
-
-    if (document.body.classList.contains("dark-theme")) {
-        //   Troca a logo para a versão escura
-        logo.src = "src/Logo modo escuro.png";
-        logof.src = "src/Logo modo escuro.png"
-    } else {
-        // Troca a logo para a versão clara
-        logo.src = "src/Logo modo claro.png";
-        logof.src = "src/Logo modo claro.png";
-    }
+    
+    
+        if (document.body.classList.contains("dark-theme")) {
+            //   Troca a logo para a versão escura
+            logo.src = "src/Logo modo escuro.png";
+            // logof.src = "src/Logo modo escuro.png"
+        } else {
+            // Troca a logo para a versão clara
+            logo.src = "src/Logo modo claro.png";
+            // logof.src = "src/Logo modo claro.png";
+        }
 }
